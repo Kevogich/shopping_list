d4707164258a786806b839e49fd8df78f981d11e
andela-jkimani <jackline.kimani@andela.com>
2017-05-05 22:10:33 +0300

    Initial setup

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => (
+  <div className="container">
+    <div className="text-lg-center">
+      <div className="jumbotron">
+        <h1 className="display-3">Shopping List</h1>
+        <p>App to manage your shopping list while learning React, Redux and Firebase</p>
+      </div>
+    </div>
+  </div>
+  );
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+const Main = () => (
+  <div>
+    <App />
+  </div>
+  );
+
+ReactDOM.render(<Main />, document.getElementById('app'));
